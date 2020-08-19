b75fea0abe41917de9c78745fb076408c76fbef7
Narupon Phantham <tacko@Narupons-MacBook-Pro.local>
2020-08-19 10:36:13 +0700

    update

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from messages import send
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/compliment/<phone_number>')
 def compliment(phone_number):
     send.compliment(phone_number)
-    return f'Thanks for stopping by, we are sending {phone_number} a compliment now!'
+    db_url = os.getenv('DB_URL')
+    return f'Thanks for stopping by, we are sending {phone_number} a compliment now! db_url={db_url}'
 
 @app.route('/greeting/<phone_number>')
 def greeting(phone_number):
     send.greeting(phone_number)
-    return f'Thanks for stopping by, we are sending {phone_number} a greeting now!'
+    db_url = os.getenv('DB_URL')
+    return f'Thanks for stopping by, we are sending {phone_number} a greeting now! db_url={db_url}'
--- messages/send.py
+++ messages/send.py
@@ -1,9 +1,17 @@
 # send.py
-import logging
+import logging, requests, json
 
 def compliment(phone_number):
     print('compliment', phone_number)
 
 def greeting(phone_number):
     print('greeting', phone_number)
-    logging.debug('debug')
+
+    url = 'http://localhost:8069/api/crm.lead/1'
+    # data = json.loads('')
+
+    resp = requests.get(
+        url = url,
+        headers = {'Access-Token': 'a9a4030acc3d6aaf753e1b0228af3f1d2f04e931'},
+    )
+    print(resp.json())
